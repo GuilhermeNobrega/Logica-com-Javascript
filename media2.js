416188332952b4ab5c597b2317bccdd9f6288dad
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-03 11:30:01 -0300

    Add files via upload

---
+++ media2.js
@@ -0,0 +1,6 @@
+var A = Number(window.prompt("Digite sua primeira nota"));
+var B = Number(window.prompt("Digite sua segunda nota"));
+var C = Number(window.prompt("Digite sua terceira nota"));
+media = (A*2+B*3+C*5);
+resultado = media/10
+window.alert(`MEDIA = ${resultado}`)
