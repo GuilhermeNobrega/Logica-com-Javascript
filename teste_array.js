28708dbdb06968b218d4ba4ff6852a69ed9c5fb9
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-03 11:33:57 -0300

    Update teste_array.js

--- teste_array.js
+++ teste_array.js
@@ -2,7 +2,8 @@ var lista_de_valores = [1,2,3,4,5,1,2];
 for(let i=0;i<lista_de_valores.length-1;i++){
     for(let j=i+1;j<lista_de_valores.length;j++){
         if(lista_de_valores[i]==lista_de_valores[j]){
-            console.log("Elementos duplicados no array");
+            console.log("Elementos duplicados no array "
+            + lista_de_valores[i]);
         }
     }
 }
