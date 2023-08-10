bf3d08d482ef64fd040f075f4c331137817e4383
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-10 10:49:39 -0300

    Create random_values.js

---
+++ random_values.js
@@ -0,0 +1,11 @@
+let number_random = Math.floor(Math.random()*3)
+
+let letras = 'abc';
+
+let letra = letras.charAt(number_random)
+
+let valor_aleatorio = Math.floor(Math.random()*10)
+
+let resultado = valor_aleatorio.toString()+letra
+console.log(resultado)
+
